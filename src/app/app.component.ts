77a06a2fbd259de3a69a15622e1fbe8e59497702
Daniel Schröder <daniel.schroeder@endobit.de>
2018-04-30 02:15:40 +0200

    cdk removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { MediaMatcher } from '@angular/cdk/layout';
 import {routes} from './app.routes'
 import { Routes } from '@angular/router';
 import { MatIconRegistry } from '@angular/material';
+import { ObservableMedia, MediaChange  } from "@angular/flex-layout";
+import { Subscribable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,18 +15,19 @@ export class AppComponent implements OnDestroy {
   mobileQuery: MediaQueryList;
   title = 'Angular Tutorial';
   links: Routes;
+  observableMedia: ObservableMedia
+  watcher: Subscription
+  mediachange: MediaChange
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private iconReg: MatIconRegistry) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this.iconReg.addSvgIcon("logo","assets/images/angular.svg");
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
+  constructor(private obsmed: ObservableMedia ,changeDetectorRef: ChangeDetectorRef) {
+    this.watcher = obsmed.subscribe(n => {
+      this.mediachange = n;
+    });
     this.links = routes.filter(p => p.path !== '' && p.path !== '\*\*');
-    console.log(this.links);
   }
 
   ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
+    this.watcher.unsubscribe();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { MaterialModule } from './material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HighlightModule } from "ngx-highlightjs";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
-import { LayoutModule } from '@angular/cdk/layout';
 import {routes} from './app.routes'
 import { PerfectScrollbarModule } from "ngx-perfect-scrollbar";
 @NgModule({
@@ -19,7 +18,6 @@ import { PerfectScrollbarModule } from "ngx-perfect-scrollbar";
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    LayoutModule,
     PerfectScrollbarModule,
     RouterModule.forRoot(routes),
     HighlightModule.forRoot({ theme: 'atom-one-dark'})
